797906d6ab66f7ffdea0f079a551ea1f8f672d0d
jeronimo scarpa labrador <jeronimos@JeronimoS.local>
2020-11-02 19:27:24 -0300

    forecast

---
+++ src/components/ForecastChart/ForecastChart.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ForecastChart from './ForecastChart'
+
+export default {
+    title: 'ForecastChart',
+    component: ForecastChart
+}
+
+export const forecastChartExample = () => (<ForecastChart  />)
